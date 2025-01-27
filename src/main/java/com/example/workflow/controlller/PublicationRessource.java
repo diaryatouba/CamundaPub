2f860d39ccf47fa9f10a0b2d6f5f42be0b078c05
diaryatouba <mariamabousso@groupeisi.com>
2025-01-27 20:59:24 +0000

    4

--- src/main/java/com/example/workflow/controlller/PublicationRessource.java
+++ src/main/java/com/example/workflow/controlller/PublicationRessource.java
@@ -1,7 +1,6 @@
 package com.example.workflow.controlller;
 
 import com.example.workflow.service.PublicationService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
